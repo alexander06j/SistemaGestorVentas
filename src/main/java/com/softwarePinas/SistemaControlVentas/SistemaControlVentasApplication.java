03b8ecf01f67f17cba1f97a7ac3f04fbc893dfd0
alexander06j <alexander050601@gmail.com>
2026-09-19 14:12:11 -0500

    docs: inicializar proyecto

---
+++ src/main/java/com/softwarePinas/SistemaControlVentas/SistemaControlVentasApplication.java
@@ -0,0 +1,13 @@
+package com.softwarePinas.SistemaControlVentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaControlVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaControlVentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwarePinas/SistemaControlVentas/SistemaControlVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarePinas.SistemaControlVentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaControlVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
