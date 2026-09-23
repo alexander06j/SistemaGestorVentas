f458ef74ee8c31bb8f7df0fb290247990b75f90a
alexander06j <alexander050601@gmail.com>
2026-09-22 22:39:00 -0500

    docs: añadir controllers, Crud de ProductoController

---
+++ src/main/java/com/softwarePinas/SistemaControlVentas/controller/SucursalController.java
@@ -0,0 +1,9 @@
+package com.softwarePinas.SistemaControlVentas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/sucursales")
+public class SucursalController {
+}
