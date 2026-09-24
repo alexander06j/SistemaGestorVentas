3268833867f781e046c17a28d3196147393c6b2a
alexander06j <alexander050601@gmail.com>
2026-09-23 22:03:53 -0500

    docs: añadir Endpoints de VentaController

--- src/main/java/com/softwarePinas/SistemaControlVentas/controller/VentaController.java
+++ src/main/java/com/softwarePinas/SistemaControlVentas/controller/VentaController.java
@@ -1,9 +1,43 @@
 package com.softwarePinas.SistemaControlVentas.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.softwarePinas.SistemaControlVentas.dto.VentaDTO;
+import com.softwarePinas.SistemaControlVentas.service.IVentaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/ventas")
 public class VentaController {
+
+    @Autowired
+    private IVentaService ventaService;
+
+    //POST
+    @PostMapping
+    public ResponseEntity<VentaDTO> save(@RequestBody VentaDTO ventaDTO){
+        VentaDTO ventaDTOFinal = ventaService.save(ventaDTO);
+        return new ResponseEntity<>(ventaDTOFinal, HttpStatus.CREATED);
+    }
+
+    //GET
+    @GetMapping
+    public ResponseEntity<List<VentaDTO>> findAll(){
+        return ResponseEntity.ok(ventaService.findAll());
+    }
+
+    //PUT
+    public ResponseEntity<VentaDTO> update(@PathVariable Long id,
+                                           @RequestBody VentaDTO ventaDTO){
+        return ResponseEntity.ok(ventaService.update(id, ventaDTO));
+    }
+
+    //DELETE
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        ventaService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
